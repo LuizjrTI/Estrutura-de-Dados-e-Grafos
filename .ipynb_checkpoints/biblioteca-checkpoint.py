06446bfdb01cd5832fb43b4cce35f704ad62e889
Your Name <you@example.com>
2020-11-07 18:09:08 -0300

    Alterações finais e mudadança para o vs

---
+++ .ipynb_checkpoints/biblioteca-checkpoint.py
@@ -0,0 +1,12 @@
+'''
+
+'''
+
+def mensagem_texto():
+    mensagem = input()
+    return mensagem
+
+
+def numero_float():
+    num_float = float(input())
+    return num_float
---
+++ biblioteca.py
@@ -0,0 +1,7 @@
+def mensagem_texto():
+    mensagem = input()
+    return mensagem
+
+def numero_float():
+    num_float = float(input())
+    return num_float
